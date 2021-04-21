b4dcaac757e5eb8edef4824dd7fbd0037dc1388c
Dennis <dmarlo96@live.ca>
2021-04-21 19:28:33 -0400

    User Model & Validation

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const { body, validationResult } = require('express-validator');
+
+const User = require('../models/User');
 
 const router = express.Router();
 
 // POST: api/users
 // Register a user
 // Public access
-router.post('/', (req, res) => {
-    res.send('Register a user');
-});
+router.post(
+    '/',
+    body('name', 'Please include a name').not().isEmpty(),
+    body('email', 'Please include a valid email').isEmail(),
+    body('password', 'Please include a password with 6 or more characters').isLength({min: 6}),
+    (req, res) => {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+  
+    //   User.create({
+    //     username: req.body.username,
+    //     password: req.body.password,
+    //   }).then(user => res.json(user));
+
+    res.send(req.body);
+    }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const app = express();
 // Connect to Database
 connectDB();
 
+// Init middleware
+app.use(express.json({ }));
+
 app.get('/', (req, res) => res.json({ msg: 'Welcome to the ContactKeeper API...'}));
 
 // Route Defined
