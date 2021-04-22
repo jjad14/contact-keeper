27e421c7704067d1a75dd0edd6128b04629888eb
Dennis <dmarlo96@live.ca>
2021-04-21 21:36:37 -0400

    Add Contact

--- routes/contacts.js
+++ routes/contacts.js
@@ -28,21 +28,53 @@ router.get('/', auth, async (req, res) => {
 // POST: api/contacts
 // Add new contact
 // Private access
-router.post('/', (req, res) => {
-    res.send('Add Contact');
+router.post(
+    '/', 
+    [ auth, [
+        body('name', 'Name is required').not().isEmpty()
+    ]], 
+    async (req, res) => {
+      // Finds the validation errors in this request and wraps them in an object with handy functions
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+    
+      // get contact data from body
+    const {name, email, phone, type} = req.body;
+
+    try {
+        // create new contact object
+        const newContact = new Contact({
+            name,
+            email,
+            phone,
+            type,
+            user: req.user.id
+        });
+
+        // save to db
+        const contact = await newContact.save();
+
+        // return newly created contact
+        res.json(contact);
+
+    } catch (error) {
+        res.status(500).send('Server Error');
+    }
 });
 
 // PUT: api/contacts/:id
 // Update contact
 // Private access
-router.put('/:id', (req, res) => {
+router.put('/:id', auth, (req, res) => {
     res.send('Update Contact');
 });
 
 // DELETE: api/contacts/:id
 // Delete contact
 // Private access
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, (req, res) => {
     res.send('Delete Contact');
 });
 
